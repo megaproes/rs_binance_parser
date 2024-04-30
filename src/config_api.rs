66e914e488f471f7fa636866baf38641525653c2
alex <sasha.borchenko@gmail.com>
2024-04-30 14:15:43 +0300

    Giving more handling

--- src/config_api.rs
+++ src/config_api.rs
@@ -1,5 +1,4 @@
-use serde::{Deserialize};
-
+use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
 
@@ -14,7 +13,27 @@ impl Config {
         let mut file = File::open("src/config.json")?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
-        let config = serde_json::from_str(&contents)?;
+        let config: Config = serde_json::from_str(&contents)?;
+
+        let api_key_len = config.api_key.chars().count();
+        let secret_key_len = config.secret_key.chars().count();
+        
+        let mut errors = Vec::new();
+        
+        if api_key_len < 64 {
+            errors.push(format!("API key should be 64 characters, but it has {} characters", api_key_len));
+        }
+        if secret_key_len < 64 {
+            errors.push(format!("Secret key should be 64 characters, but it has {} characters", secret_key_len));
+        }
+        
+        if !errors.is_empty() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                errors.join(", and "),
+            ));
+        }
+
         Ok(config)
     }
 }
